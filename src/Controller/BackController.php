860bf98523a479a6d63f0ab42765ed1b30e6829a
ahmedya44 <ahmed.yaich@esprit.tn>
2026-02-16 16:00:05 +0100

    template

---
+++ src/Controller/BackController.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Error\LoaderError;
+
+#[Route('/admin', name: 'back_')]
+final class BackController extends AbstractController
+{
+    #[Route('', name: 'dashboard', methods: ['GET'])]
+    public function dashboard(): RedirectResponse
+    {
+        return $this->redirectToRoute('back_page', ['path' => 'index']);
+    }
+
+    #[Route('/{path}.html', name: 'page', requirements: ['path' => '[A-Za-z0-9_\-/]+'], methods: ['GET'])]
+    public function page(string $path): Response
+    {
+        if (str_contains($path, '..')) {
+            throw $this->createNotFoundException();
+        }
+
+        $template = sprintf('back/%s.html.twig', $path);
+
+        try {
+            return $this->render($template);
+        } catch (LoaderError) {
+            throw $this->createNotFoundException(sprintf('Back template "%s" was not found.', $template));
+        }
+    }
+}
